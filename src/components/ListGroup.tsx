38be9d3fdff8a1a29af86ae7d99f8248a7776de2
keemeayo <keemeayo@gmail.com>
2025-02-02 17:30:35 +0000

    modified:   src/components/ListGroup.tsx

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,16 +1,27 @@
+import { useState } from "react";
+
 function ListGroup() {
   let items = ["New York", "Los Angeles", "Chicago", "Houston"];
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
 
   return (
+
     <>
       <h1>List</h1>
       {items.length === 0 && <p>No item found!</p>}
       <ul className="list-group">
-        {items.map((item, index) => (
+        {items.map((item) => (
           <li
-            className="list-group-item"
+            className={
+              selectedIndex === items.indexOf(item)
+                ? "list-group-item active"
+                : "list-group-item"
+            }
             key={item}
-            onClick={() => console.log(item, index)}
+            onClick={() => {
+              setSelectedIndex(items.indexOf(item));
+            }}
           >
             {item}
           </li>
